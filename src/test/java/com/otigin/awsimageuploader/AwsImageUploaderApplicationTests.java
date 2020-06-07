5963a0028ba16a04ff6397f41a093a0858db6b38
jirwincline <jci@pdx.edu>
2020-06-06 17:07:28 -0700

    initialize project

---
+++ src/test/java/com/otigin/awsimageuploader/AwsImageUploaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otigin.awsimageuploader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsImageUploaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
