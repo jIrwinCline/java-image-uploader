f6940ab7a551c20621b425a6569f7142c8f72869
jirwincline <jci@pdx.edu>
2020-06-07 23:47:14 -0700

    adit react app and added a fetch call function to imlement with backend

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
-//removed
-//removed
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const UserProfiles = () => {
+  const fetchUserProfiles = () => {
+    axios.get("http://localhost:8080/api/v1/user-profile").then((res) => {
+      console.log(res);
+    });
+  };
+  useEffect(() => {
+    fetchUserProfiles();
+  }, []);
+  return <h1>Hello</h1>;
+};
 
 function App() {
   return (
     <div className="App">
-      <h1>Trimmed React App</h1>
+      <UserProfiles />
     </div>
   );
 }
