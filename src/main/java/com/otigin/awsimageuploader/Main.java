ca5fbb4cd950ca30dd279181df53bc89d9d07f1f
jirwincline <jci@pdx.edu>
2020-06-07 23:05:57 -0700

    add a controller post method

--- src/main/java/com/otigin/awsimageuploader/Main.java
+++ src/main/java/com/otigin/awsimageuploader/Main.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AwsImageUploaderApplication {
+public class Main {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AwsImageUploaderApplication.class, args);
+		SpringApplication.run(Main.class, args);
 	}
 
 }
--- src/main/java/com/otigin/awsimageuploader/profile/UserProfileService.java
+++ src/main/java/com/otigin/awsimageuploader/profile/UserProfileService.java
@@ -1,5 +1,26 @@
 package com.otigin.awsimageuploader.profile;
 
+import java.util.List;
+import java.util.UUID;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
 public class UserProfileService {
 
+  private final UserProfileDataAccessService userProfileDataAccessService;
+
+  @Autowired
+  public UserProfileService(
+      UserProfileDataAccessService userProfileDataAccessService) {
+    this.userProfileDataAccessService = userProfileDataAccessService;
+  }
+
+  List<UserProfile> getUserProfiles() {
+    return userProfileDataAccessService.getUserProfiles();
+  }
+
+  public void uploadUserProfileImage(UUID userProfileId, MultipartFile file) {
+  }
 }
